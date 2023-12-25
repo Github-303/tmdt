6f91d87fdc547e1fd0c163e4bed33aa807945d43
Github-303 <emlaem12345678901@gmail.com>
2023-12-25 14:32:47 +0700

    login register

--- login.js
+++ login.js
@@ -31,14 +31,38 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
   }
-  function validateEmail(email) {
-    // Basic email validation
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
 
-  function validatePassword(password) {
-    // Basic password validation (minimum length)
-    return password.length >= 6;
+  var registerForm = document.querySelector(".register-form");
+
+  if (registerForm) {
+    registerForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      var username = registerForm.elements.firstname.value;
+      var email = registerForm.elements.email.value;
+      var password = registerForm.elements.password.value;
+
+      // Make an AJAX request to the server
+      fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            alert(data.message);
+            // Redirect or perform other actions upon successful registration
+          } else {
+            alert(data.message);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("An error occurred. Please try again.");
+        });
+    });
   }
 });
--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  // Thêm các xử lý khác nếu cần
+  process.exit(1); // Dừng quá trình Node.js với mã lỗi 1
+});
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -7,7 +13,6 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Connect to MongoDB (replace 'your_database_url' with your actual MongoDB URL)
 mongoose.connect(
   "mongodb+srv://admin:<admin>@exception.wrnvwew.mongodb.net/?retryWrites=true&w=majority",
   {
@@ -16,51 +21,50 @@ mongoose.connect(
   }
 );
 
-// Define a user schema (replace with your actual user schema)
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
 const User = mongoose.model("User", {
   email: String,
   password: String,
 });
 
-// Handle login requests
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
   try {
-    // Check if the user exists in the database
+    const { email, password } = req.body;
     const user = await User.findOne({ email, password });
 
     if (user) {
       res.json({ success: true, message: "Login successful!" });
     } else {
-      res
-        .status(401)
-        .json({ success: false, message: "Invalid email or password." });
+      res.status(401).json({ success: false, message: "Invalid email or password." });
     }
   } catch (error) {
-    console.error(error);
+    console.error("Login error:", error);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 });
-// Handle registration requests
-app.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
 
+app.post("/register", async (req, res) => {
   try {
-    // Check if the user already exists in the database
+    const { username, email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       res.status(400).json({ success: false, message: "Email already in use." });
     } else {
-      // Create a new user in the database
       const newUser = new User({ username, email, password });
       await newUser.save();
-
       res.json({ success: true, message: "Registration successful!" });
     }
   } catch (error) {
-    console.error(error);
+    console.error("Registration error:", error);
     res.status(500).json({ success: false, message: "Internal server error." });
   }
 });
